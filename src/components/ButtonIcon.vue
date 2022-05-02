9b3301c4c440a2ca07afe41a19840a8b2a2b0465
K-shigehito <example@test.com>
2022-05-02 12:50:58 +0900

    fix: 各アイコンコンポーネントを動的に取得

--- src/components/ButtonIcon.vue
+++ src/components/ButtonIcon.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import BaseIcon from './icons/BaseIcon.vue';
 import IconSearch from './icons/IconSearch.vue';
 import IconEdit from './icons/IconEdit.vue';
@@ -14,12 +15,19 @@ const props = withDefaults(defineProps<Props>(), {
   width: 10,
   height: 10,
 });
+
+const icons = {
+  IconSearch,
+  IconEdit,
+  IconFavorite,
+};
+const currentIcon = computed(() => {
+  return `Icon${props.icon.charAt(0).toUpperCase() + props.icon.slice(1)}`;
+});
 </script>
 
 <template>
   <BaseIcon :width="props.width" :height="props.height">
-    <IconSearch v-if="props.icon === 'search'" />
-    <IconEdit v-else-if="props.icon === 'edit'" />
-    <IconFavorite v-else-if="props.icon === 'favorite'"></IconFavorite>
+    <component :is="icons[currentIcon]"></component>
   </BaseIcon>
 </template>
